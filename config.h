d0eb121ceedb53503997ef4fc848ea72e5f5e332
chaosone <solomoon186@gmail.com>
2020-10-14 23:12:37 +0800

    change font to firecode

--- config.h
+++ config.h
@@ -5,8 +5,8 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Liberation Mono:pixelsize=25:antialias=true:autohint=true";
-//static char *font = "Source Code Pro:pixelsize=25:antialias=true:autohint=true";
+//static char *font = "Liberation Mono:pixelsize=25:antialias=true:autohint=true";
+static char *font = "Fira Code Nerd Font Mono:pixelsize=25:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
